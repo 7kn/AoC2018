b67c97b1ebbbae6343ad629888338d5df1c4ca17
7kn <tknoops@gmail.com>
2018-12-02 20:35:19 +0100

    fixed day 1-2

--- day1-2.py
+++ day1-2.py
@@ -3,18 +3,29 @@
 file = "data.txt"
 curr_freq = 0
 freq_list = []
+duplicate_freq = False
 
-with open(file) as f:
-    read_data = f.readlines()
+def run_freq_calc():
+    with open(file) as f:
+        read_data = f.readlines()
 
-for line in read_data:
-    line = line.strip('\n')
-    if line[0] == "+":
-        curr_freq += int(line[1:])
-    else:
-        curr_freq += int(line)
+    for line in read_data:
+        global curr_freq
+        global freq_list
+        global duplicate_freq
 
-    if curr_freq not in freq_list:
-        freq_list.append(curr_freq)
-    elif curr_freq in freq_list: 
-        print(curr_freq + " already exists!!")
+        line = line.strip('\n')
+        if line[0] == "+":
+            curr_freq += int(line[1:])
+        else:
+            curr_freq += int(line)
+
+        if curr_freq not in freq_list:
+            freq_list.append(curr_freq)
+        elif curr_freq in freq_list: 
+            print(str(curr_freq) + " already exists!!")
+            duplicate_freq = True
+            exit()
+
+while duplicate_freq == False:
+    run_freq_calc()
